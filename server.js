034319ad5f6004d099aa4da38d29eae4badf592a
Bruno Marcenaro <bems2588@gmail.com>
2022-03-27 14:39:58 -0400

    add post / delete

--- server.js
+++ server.js
@@ -16,10 +16,52 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("/public"));
 
-//API route
+//API GET
 app.get("/api/notes", function(req, res) {
   readFileAsync("./db/db.json", "utf8").then(function(data) {
     notes = [].concat(JSON.parse(data))
     res.json(notes);
   })
 })
+
+//API POST
+app.post("api/notes", function(req,res) {
+  const note = req.body;
+  readFileAsync("./db/db.json", "utf8").then(function(data) {
+    note.id = notes.length + 1
+    notes.push(note);
+    return notes
+  }).then(function(notes) {
+    writeFileAsync("./db/db.json", JSON.stringify(notes))
+    res.json(note);
+  })
+});
+
+//API DELETE
+app.delete("api/notes/:id", function(req, res) {
+  const idToDelete = parseInt(req, params.id);
+  readFileAsync("./db/db.json", "utf8").then(function(data) {
+    const notes = [].concat(JSON.parse(data));
+    const newNotesData = []
+    for (let i = 0; i < notes.length; i++) {
+      if(idToData !== notes[i].id) {
+        newNotesData.push(notes[i])
+      }
+    }
+    return newNotesData
+  }).then(function(notes) {
+    writeFileAsync("./db/db.json", JSON.stringify(notes))
+    res.send('saved!');
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
