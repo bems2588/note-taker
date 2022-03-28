7fc114f6cfd14637dc627b3dbdbac4df02f00e06
Bruno Marcenaro <bems2588@gmail.com>
2022-03-28 12:27:15 -0400

    add app routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,18 @@
+const app = require("express").Router()
+const path = require("path");
+
+//HTML routes
+app.get("/notes", function(req,res) {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-const express = require("express");
-const path = require("path");
 const fs = require("fs");
 const db = require("./db/db.json");
 const util = require("util");
@@ -13,7 +11,7 @@ const PORT = process.env.PORT || 8000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("./public"));
+app.use(express.static('./public'));
 
 //API GET
 app.get("/api/notes", function(req, res) {
@@ -55,18 +53,7 @@ app.delete("api/notes/:id", function(req, res) {
 })
 
 
-//HTML routes
-app.get("/notes", function(req,res) {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
